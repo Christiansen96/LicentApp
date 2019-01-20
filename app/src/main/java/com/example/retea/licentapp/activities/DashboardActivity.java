0e4b6ef4bdf0df90074cfe84f2961539fe289144
Cristian <retea.cristian96@gmail.com>
2019-01-20 17:56:33 +0200

    adaugat utils goala

--- app/src/main/java/com/example/retea/licentapp/activities/DashboardActivity.java
+++ app/src/main/java/com/example/retea/licentapp/activities/DashboardActivity.java
@@ -5,6 +5,8 @@ import android.location.Geocoder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.retea.licentapp.LicentApplication;
@@ -19,14 +21,30 @@ public class DashboardActivity extends AppCompatActivity {
     private static final String TAG = "DashboardActivity";
 
     private TextView AddressTextView;
+    private Button FavouritesButton;
+    private Button NearbyButton;
+    private Button SearchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
         Log.d(TAG, "onCreate: entered");
+
         AddressTextView = findViewById(R.id.AddressTextViewId);
+        FavouritesButton = findViewById(R.id.FavouritesButtonId);
+        NearbyButton = findViewById(R.id.NearbyButtonId);
+        SearchButton = findViewById(R.id.SearchButtonId);
+
         getDeviceAddress();
+
+        FavouritesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
 
     }
 
---
+++ app/src/main/java/com/example/retea/licentapp/utils/Utils.java
@@ -0,0 +1,4 @@
+package com.example.retea.licentapp.utils;
+
+public class Utils {
+}
