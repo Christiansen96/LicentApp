c711817ddc65fb96f815023259fe8e6d96622947
Cristian <retea.cristian96@gmail.com>
2019-07-26 17:12:15 +0300

    expandable and contractable map

--- app/src/main/java/com/example/retea/licentapp/fragments/NearbyFragment.java
+++ app/src/main/java/com/example/retea/licentapp/fragments/NearbyFragment.java
@@ -1,9 +1,11 @@
 package com.example.retea.licentapp.fragments;
 
 import android.Manifest;
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
@@ -15,6 +17,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import com.example.retea.licentapp.LicentApplication;
 import com.example.retea.licentapp.R;
@@ -24,22 +27,30 @@ import com.example.retea.licentapp.models.ClusterMarker;
 import com.example.retea.licentapp.models.GeologicalPosition;
 import com.example.retea.licentapp.models.Provider;
 import com.example.retea.licentapp.utils.MyClusterManagerRenderer;
+import com.example.retea.licentapp.utils.ViewWeightAnimationWrapper;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.maps.android.clustering.ClusterManager;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class NearbyFragment extends Fragment implements OnMapReadyCallback, ProviderListAdapter.OnItemClickListener {
+public class NearbyFragment extends Fragment implements OnMapReadyCallback, ProviderListAdapter.OnItemClickListener, View.OnClickListener {
 
     private static final String TAG = "NearbyFragment";
+    private static final int MAP_LAYOUT_STATE_CONTRACTED = 0;
+    private static final int  MAP_LAYOUT_STATE_EXPANDED = 1;
+
 
     protected MapView mMapView;
+    private RelativeLayout mMapContainer;
+    private RecyclerView providerListRecyclerView;
+
     private GoogleMap mGoogleMap;
     private LatLngBounds mMapBoundary;
     private GeologicalPosition mGeologicalPosition;
@@ -47,6 +58,8 @@ public class NearbyFragment extends Fragment implements OnMapReadyCallback, Prov
     private ClusterManager mClusterManager;
     private MyClusterManagerRenderer mClusterManagerRenderer;
     private ArrayList<ClusterMarker> mClusterMarkers;
+    private int mMapLayoutState = 0;
+
 
     @Nullable
     @Override
@@ -55,13 +68,15 @@ public class NearbyFragment extends Fragment implements OnMapReadyCallback, Prov
         mMapView = view.findViewById(R.id.fragment_embedded_map_view_mapview);
         mMapView.onCreate(savedInstanceState);
         mMapView.getMapAsync(this);
+        mMapContainer = view.findViewById(R.id.map_container);
+        view.findViewById(R.id.btn_full_screen_map).setOnClickListener(this);
 
         List<Provider> providers = LicentApplication.getProviders();
 
-        RecyclerView providerListRecyclerView = (RecyclerView) view.findViewById(R.id.ProviderListRecyclerView);
+        providerListRecyclerView = (RecyclerView) view.findViewById(R.id.ProviderListRecyclerView);
         providerListRecyclerView.setHasFixedSize(true);
 
-        ProviderListAdapter providerListAdapter = new ProviderListAdapter(providers,this);
+        ProviderListAdapter providerListAdapter = new ProviderListAdapter(providers, this);
         layoutManager = new LinearLayoutManager(this.getContext());
         providerListRecyclerView.setLayoutManager(layoutManager);
 
@@ -69,18 +84,17 @@ public class NearbyFragment extends Fragment implements OnMapReadyCallback, Prov
         providerListRecyclerView.setItemAnimator(new DefaultItemAnimator());
 
 
-
         return view;
     }
 
-    private void addMapMarkers(){
+    private void addMapMarkers() {
 
-        if(mGoogleMap != null){
+        if (mGoogleMap != null) {
 
-            if(mClusterManager == null){
+            if (mClusterManager == null) {
                 mClusterManager = new ClusterManager<ClusterMarker>(getActivity().getApplicationContext(), mGoogleMap);
             }
-            if(mClusterManagerRenderer == null){
+            if (mClusterManagerRenderer == null) {
                 mClusterManagerRenderer = new MyClusterManagerRenderer(
                         getActivity(),
                         mGoogleMap,
@@ -89,29 +103,29 @@ public class NearbyFragment extends Fragment implements OnMapReadyCallback, Prov
                 mClusterManager.setRenderer(mClusterManagerRenderer);
             }
 
-            for(Provider provider: LicentApplication.getProviders()){
+            for (Provider provider : LicentApplication.getProviders()) {
 
                 Log.d(TAG, "addMapMarkers: location: " + provider.getProviderGeologicalPosition().toString());
-                try{
+                try {
                     String snippet = "";
-                    if(provider.getCategory().equals("Stomatologie")){
+                    if (provider.getCategory().equals("Stomatologie")) {
                         snippet = "Stomatologie";
-                    }else if(provider.getCategory().equals("Curatenie")){
+                    } else if (provider.getCategory().equals("Curatenie")) {
                         snippet = "Curatenie";
-                    }else if(provider.getCategory().equals("Masaj")){
+                    } else if (provider.getCategory().equals("Masaj")) {
                         snippet = "Masaj";
-                    } else if(provider.getCategory().equals("Instalator")){
+                    } else if (provider.getCategory().equals("Instalator")) {
                         snippet = "Instalator";
-                    } else if(provider.getCategory().equals("Mobila")){
+                    } else if (provider.getCategory().equals("Mobila")) {
                         snippet = "Mobila";
                     }
 
 
                     int providerImage = R.drawable.powered_by_google_light; // set the default avatar
-                    try{
+                    try {
                         providerImage = provider.getImage();
-                    }catch (NumberFormatException e){
-                        Log.d(TAG, "addMapMarkers: no avatar for " + provider.getName()+ ", setting default.");
+                    } catch (NumberFormatException e) {
+                        Log.d(TAG, "addMapMarkers: no avatar for " + provider.getName() + ", setting default.");
                     }
                     ClusterMarker newClusterMarker = new ClusterMarker(
                             new LatLng(provider.getProviderGeologicalPosition().getLatitude(), provider.getProviderGeologicalPosition().getLongitude()),
@@ -121,11 +135,12 @@ public class NearbyFragment extends Fragment implements OnMapReadyCallback, Prov
                             provider
 
                     );
+
                     mClusterManager.addItem(newClusterMarker);
                     mClusterMarkers.add(newClusterMarker);
 
-                }catch (NullPointerException e){
-                    Log.e(TAG, "addMapMarkers: NullPointerException: " + e.getMessage() );
+                } catch (NullPointerException e) {
+                    Log.e(TAG, "addMapMarkers: NullPointerException: " + e.getMessage());
                 }
 
             }
@@ -193,20 +208,19 @@ public class NearbyFragment extends Fragment implements OnMapReadyCallback, Prov
         addMapMarkers();
 
 
-
     }
 
-    private void setCameraView(GeologicalPosition geologicalPosition){
+    private void setCameraView(GeologicalPosition geologicalPosition) {
 
-        double bottomBoundary = geologicalPosition.getLatitude()- .005;
+        double bottomBoundary = geologicalPosition.getLatitude() - .005;
         double leftBoundary = geologicalPosition.getLongitude() - .005;
         double topBoundary = geologicalPosition.getLatitude() + .005;
         double rightBoundary = geologicalPosition.getLongitude() + .005;
-        mMapBoundary = new LatLngBounds(new LatLng(bottomBoundary,leftBoundary),new LatLng(topBoundary,rightBoundary));
+        mMapBoundary = new LatLngBounds(new LatLng(bottomBoundary, leftBoundary), new LatLng(topBoundary, rightBoundary));
 
         Log.d(TAG, "moveCamera: moving camera to: lat:" + geologicalPosition.getLatitude() + ", lng: " + geologicalPosition.getLongitude());
-        LatLng latLng = new LatLng(geologicalPosition.getLatitude(),geologicalPosition.getLongitude());
-        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(mMapBoundary,0));
+        LatLng latLng = new LatLng(geologicalPosition.getLatitude(), geologicalPosition.getLongitude());
+        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(mMapBoundary, 0));
 
     }
 
@@ -216,5 +230,62 @@ public class NearbyFragment extends Fragment implements OnMapReadyCallback, Prov
         Intent intent = new Intent(this.getContext(), ProviderActivity.class);
         startActivity(intent);
         Log.d(TAG, "onItemClick: " + provider.toString());
+    }
+
+    private void expandMapAnimation(){
+        ViewWeightAnimationWrapper mapAnimationWrapper = new ViewWeightAnimationWrapper(mMapContainer);
+        ObjectAnimator mapAnimation = ObjectAnimator.ofFloat(mapAnimationWrapper,
+                "weight",
+                50,
+                100);
+        mapAnimation.setDuration(300);
+
+        ViewWeightAnimationWrapper recyclerAnimationWrapper = new ViewWeightAnimationWrapper(providerListRecyclerView);
+        ObjectAnimator recyclerAnimation = ObjectAnimator.ofFloat(recyclerAnimationWrapper,
+                "weight",
+                50,
+                0);
+        recyclerAnimation.setDuration(300);
+
+        recyclerAnimation.start();
+        mapAnimation.start();
+    }
+
+    private void contractMapAnimation(){
+        ViewWeightAnimationWrapper mapAnimationWrapper = new ViewWeightAnimationWrapper(mMapContainer);
+        ObjectAnimator mapAnimation = ObjectAnimator.ofFloat(mapAnimationWrapper,
+                "weight",
+                100,
+                50);
+        mapAnimation.setDuration(300);
+
+        ViewWeightAnimationWrapper recyclerAnimationWrapper = new ViewWeightAnimationWrapper(providerListRecyclerView);
+        ObjectAnimator recyclerAnimation = ObjectAnimator.ofFloat(recyclerAnimationWrapper,
+                "weight",
+                0,
+                50);
+        recyclerAnimation.setDuration(300);
+
+        recyclerAnimation.start();
+        mapAnimation.start();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_full_screen_map:{
+
+                if(mMapLayoutState == MAP_LAYOUT_STATE_CONTRACTED){
+                    mMapLayoutState = MAP_LAYOUT_STATE_EXPANDED;
+                    expandMapAnimation();
+                }
+                else if(mMapLayoutState == MAP_LAYOUT_STATE_EXPANDED){
+                    mMapLayoutState = MAP_LAYOUT_STATE_CONTRACTED;
+                    contractMapAnimation();
+                }
+                break;
+            }
+
+        }
     }
 }
