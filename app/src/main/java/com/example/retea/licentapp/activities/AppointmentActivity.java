e9936997d724e7ef3f63fdddc8e8932f3287d840
Cristian <retea.cristian96@gmail.com>
2019-08-02 01:07:23 +0300

    choose date and time for appointment

---
+++ app/src/main/java/com/example/retea/licentapp/activities/AppointmentActivity.java
@@ -0,0 +1,27 @@
+package com.example.retea.licentapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
+import android.widget.DigitalClock;
+import android.widget.TimePicker;
+
+import com.example.retea.licentapp.R;
+
+public class AppointmentActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appointment);
+
+
+
+
+    }
+}
--- app/src/main/java/com/example/retea/licentapp/activities/ServiceActivity.java
+++ app/src/main/java/com/example/retea/licentapp/activities/ServiceActivity.java
@@ -2,13 +2,17 @@ package com.example.retea.licentapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.example.retea.licentapp.LicentApplication;
@@ -18,10 +22,14 @@ import com.example.retea.licentapp.models.Service;
 import com.google.android.gms.maps.model.LatLng;
 import com.squareup.picasso.Picasso;
 
+import java.time.Clock;
+import java.util.Calendar;
+
 import static com.example.retea.licentapp.utils.Constants.PROVIDER_TYPE_AWAY;
 import static com.example.retea.licentapp.utils.Constants.PROVIDER_TYPE_HOME;
 
-public class ServiceActivity extends AppCompatActivity {
+public class ServiceActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener,
+        TimePickerDialog.OnTimeSetListener {
     private static final String TAG = "ServiceActivity";
 
     private TextView serviceName;
@@ -30,6 +38,10 @@ public class ServiceActivity extends AppCompatActivity {
     private Button getAppointmentButton;
     private TextView servicePrice;
     private TextView serviceDuration;
+    private TextView shownDate;
+    private TextView shownTime;
+    private Button chooseTimeButton;
+    private Button chooseDateButton;
 
     private Provider mCurrentProvider;
     private Service mCurrentService;
@@ -63,6 +75,22 @@ public class ServiceActivity extends AppCompatActivity {
         getAppointmentButton = findViewById(R.id.OpenedServiceGetAppointmentButtonId);
         servicePrice = findViewById(R.id.OpenedServicePriceId);
         serviceDuration = findViewById(R.id.OpenedServiceDurationId);
+        chooseDateButton = findViewById(R.id.ChooseDateId);
+        chooseTimeButton = findViewById(R.id.ChooseTimeId);
+        shownDate = findViewById(R.id.ShownDateId);
+        shownTime = findViewById(R.id.ShownTimeId);
+        chooseDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDatePickerDialog();
+            }
+        });
+        chooseTimeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showTimePickerDialog();
+            }
+        });
 
         Picasso.get().load(mCurrentService.getImageUri()).into(serviceImage);
         serviceName.setText(mCurrentService.getName());
@@ -76,10 +104,13 @@ public class ServiceActivity extends AppCompatActivity {
             servicePrice.setText(price);
         } else servicePrice.setText("");
 
+
         getAppointmentButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(view.getContext(), "Clicked appointment.", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(ServiceActivity.this, AppointmentActivity.class);
+                startActivity(intent);
                 Log.d(TAG, "onClick: appointment");
             }
         });
@@ -125,5 +156,52 @@ public class ServiceActivity extends AppCompatActivity {
                 }
             }
         }
+    }
+
+    private void showDatePickerDialog() {
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                this,
+                this,
+                Calendar.getInstance().get(Calendar.YEAR),
+                Calendar.getInstance().get(Calendar.MONTH),
+                Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+        );
+        datePickerDialog.show();
+    }
+
+    private void showTimePickerDialog() {
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(
+                this,
+                this,
+                Calendar.getInstance().get(Calendar.HOUR_OF_DAY),
+                Calendar.getInstance().get(Calendar.MINUTE),
+                false
+        );
+        timePickerDialog.show();
+
+    }
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+
+
+        String date = dayOfMonth + "/" + month + "/" + year;
+        shownDate.setText(date);
+
+    }
+
+    @Override
+    public void onTimeSet(TimePicker timePicker, int hour, int minute) {
+        String lMinute =  String.valueOf(minute);
+        if(minute<10){
+            lMinute = "0" + minute;
+        }
+        String lHour =  String.valueOf(hour);
+        if(hour<10){
+            lHour = "0" + hour;
+        }
+        String time = lHour + ":" + lMinute + " PM";
+        shownTime.setText(time);
     }
 }
