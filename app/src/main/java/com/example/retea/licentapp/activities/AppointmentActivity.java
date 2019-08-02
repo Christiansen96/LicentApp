0f925ad5aa11fd9fda27b6578a5d4ff0c50548c6
Cristian <retea.cristian96@gmail.com>
2019-08-02 17:12:06 +0300

    added firestore and firedb

--- app/src/main/java/com/example/retea/licentapp/activities/AppointmentActivity.java
+++ app/src/main/java/com/example/retea/licentapp/activities/AppointmentActivity.java
@@ -13,6 +13,7 @@ public class AppointmentActivity extends AppCompatActivity {
 
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
